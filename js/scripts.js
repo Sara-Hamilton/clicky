6089736c9ad1d72b779a1b1dd1ff39d49728268e
Kimberly Huynh and Sara Hamilton <kimberly@>
2018-01-16 10:47:28 -0800

    implement for each loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
-jQuery(document).ready(function(){
+$(document).ready(function(){
+  var tags = ["h1", "p", "img"];
 
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
+  tags.forEach(function(tag){
+    $(tag).click(function() {
+      alert("This is a " + tag + ".");
+      console.log(tags);
+    });
+  });
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
 
-jQuery("img").click(function() {
-  alert("This is an image.");
-});
+// $("h1").click(function() {
+//   alert("This is a header.");
+// });
+//
+// $("p").click(function() {
+//   alert("This is a paragraph.");
+// });
+//
+// $("img").click(function() {
+//   alert("This is an image.");
 });
